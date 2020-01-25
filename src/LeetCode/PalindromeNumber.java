a07cfabbbd6fc84b2e1bbb0e913a585d42ede527
Dejiang Chen <dejiang27@gmail.com>
2020-01-24 21:05:49 -0600

    lol

---
+++ src/LeetCode/PalindromeNumber.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class PalindromeNumber {
+
+    public boolean isPalindrome(int x) {
+
+        int reversted = 0;
+
+        if(x<11){
+            return false;
+        }
+
+        
+
+
+        return true;
+    }
+
+
+    public static void main(String[] args){
+        PalindromeNumber pn = new PalindromeNumber();
+
+    }
+}
