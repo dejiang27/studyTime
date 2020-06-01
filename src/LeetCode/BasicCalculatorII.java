9d2cbd920084f555e57f37c74b6199f5891aab86
Dejiang Chen <dejiang27@gmail.com>
2020-06-01 11:44:21 -0500

    lol

---
+++ src/LeetCode/BasicCalculatorII.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class BasicCalculatorII {
+
+    public int calculate(String s){
+
+        int ans = 0;
+
+
+
+        return ans;
+    }
+
+    public static void main(String[] args){
+
+        BasicCalculatorII bc = new BasicCalculatorII();
+
+        bc.calculate("3+2*2");
+
+    }
+
+
+
+
+
+}
