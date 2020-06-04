cf76dfa804acb1bb5d601f6862e3cb3189f6ea02
Dejiang Chen <dejiang27@gmail.com>
2020-06-03 19:17:08 -0500

    03/06v0.2

--- src/LeetCode/LongestCommonSubsequence.java
+++ src/LeetCode/LongestCommonSubsequence.java
@@ -21,21 +21,6 @@ public class LongestCommonSubsequence {
             }
         }
         return dp[m][n];
-        /*int n1 = text1.length(), n2 = text2.length(), ans = 0;
-        int m1 = 0, m2 = 0;
-        while(n1 > m1){
-            for(int i = m2; i < n2; i ++){
-                if(text1.charAt(m1) == text2.charAt(i)){
-                    System.out.println(i);
-                    ans ++;
-                    m2 = i + 1;
-                    break;
-                }
-            }
-            m1 ++;
-        }
-        System.out.println(ans);
-        return ans;*/
     }
 
     public static void main(String[] args){
