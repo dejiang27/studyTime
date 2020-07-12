ee37131649f7bf819765c9881984c8f9ab5baa7a
Dejiang Chen <dejiang27@gmail.com>
2020-07-11 22:12:25 -0500

    07/11/2020 v.02

--- src/LeetCode/PowerOfTwo.java
+++ src/LeetCode/PowerOfTwo.java
@@ -13,9 +13,14 @@ public class PowerOfTwo {
             return false;
         }
 
+        while(n/2 > 2 && n%2 == 0){
+            n = n/2;
+        }
 
+        if(n % 2 == 1){
+            return false;
+        }
 
-
-        return false;
+        return true;
     }
 }
---
+++ src/LeetCode/QueueReconstructionByHeight.java
@@ -0,0 +1,24 @@
+package LeetCode;
+
+public class QueueReconstructionByHeight {
+    /*
+        h -> height
+        k -> the number of people in front of this people whi have a height greater than or equal to h
+
+        Input:
+                [[7,0], [4,4], [7,1], [5,0], [6,1], [5,2]]
+
+        Output:
+                [[5,0], [7,0], [5,2], [6,1], [4,4], [7,1]]
+    */
+    public int[][] reconstructQueue(int[][] people){
+
+
+
+
+
+
+        return null;
+    }
+
+}
